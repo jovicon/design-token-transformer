8019c0c683fbe79a1cabc0cd14078f82620b3369
Automated Publisher <actions@users.noreply.github.com>
2021-02-02 22:21:44 +0000

    Automated publish: Tue Feb  2 22:21:44 UTC 2021 23c58f9e13267929bf3bf1062f27cae133eac72e

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Tue, 02 Feb 2021 22:18:43 GMT
+// Generated on Tue, 02 Feb 2021 22:21:43 GMT
 //
 
 #import <UIKit/UIKit.h>
@@ -34,7 +34,59 @@ GradientSingleWithMultipleColorStopsStops3Color,
 EffectDropShadowSingleColor,
 EffectInnerShadowMultiple0Color,
 EffectInnerShadowMultiple1Color,
-EffectInnerShadowMultiple2Color
+EffectInnerShadowMultiple2Color,
+Primary01,
+Primary02,
+Primary03,
+PrimaryTransparent10,
+PrimaryTransparent20,
+PrimaryTransparent30,
+Secondary01,
+Secondary02,
+Secondary03,
+Neutral01,
+Neutral02,
+Neutral03,
+Neutral04,
+Neutral05,
+Neutral06,
+Neutral07,
+FeedbackWarningTransparent10,
+FeedbackWarning01,
+FeedbackWarning02,
+FeedbackErrorTransparent10,
+FeedbackError01,
+FeedbackError02,
+FeedbackSuccessTransparent10,
+FeedbackSuccess01,
+FeedbackSuccess02,
+FeedbackInfoTransparent10,
+FeedbackInfo01,
+FeedbackInfo02,
+Brand01Light,
+Brand01Base,
+Brand01Dark,
+Brand02Light,
+Brand02Base,
+Brand02Dark,
+Brand03Light,
+Brand03Base,
+Brand03Dark,
+Brand04Light,
+Brand04Base,
+Brand04Dark,
+Brand05Light,
+Brand05Base,
+Brand05Dark,
+Brand06Light,
+Brand06Base,
+Brand06Dark,
+Brand07Light,
+Brand07Base,
+Brand07Dark,
+Brand08Light,
+Brand08Base,
+Brand08Dark
 };
 
 @interface StyleDictionaryColor : NSObject
